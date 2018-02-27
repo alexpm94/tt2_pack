4f529a6b8b1a3d74d7bbcb73e8a25ced013165d2
alexpm94 <j.alejandro.pena.m94@gmail.com>
2018-02-27 14:37:12 -0600

    Add names of the users to the csv

--- src/dataBase/csv.py
+++ src/dataBase/csv.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
 
     BASE_PATH=os.getcwd()
     SEPARATOR=","
+    names={'s1':'Balam','s2':'Alex','s3':'Brenda','s4':'Dani','s5':'Fortanel','s6':'Hector','s7':'Marvin','s8':'Lalo','s9':'Popoca','s10':'Rosey'}
 
     label = 0
     for dirname, dirnames, filenames in os.walk(BASE_PATH):
@@ -16,6 +17,6 @@ if __name__ == "__main__":
             subject_path = os.path.join(dirname, subdirname)
             for filename in os.listdir(subject_path):
                 abs_path = "%s/%s" % (subject_path, filename)
-                file_csv.write("%s%s%d\n" % (abs_path, SEPARATOR, label))
+                file_csv.write("%s%s%d%s%s\n" % (abs_path, SEPARATOR, label, SEPARATOR, names[subdirname]))
             label = label + 1
 file_csv.close()
