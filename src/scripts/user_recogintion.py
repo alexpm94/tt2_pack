3ff35f61201dae62c1ae8ed2bf162abaa4f7091f
alexpm94 <j.alejandro.pena.m94@gmail.com>
2018-03-09 10:16:59 -0600

    Add user_recogintion node and .pkl file

---
+++ src/scripts/user_recogintion.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python
+# it requires "chmod +x mypythonscript.py" to be called by ROS
+
+import rospy
+from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
+from std_msgs.msg import String
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import numpy as np
+import time
+import math
+from numpy import *
+import os.path
+import Classification
+
+#Constants
+path_user=rospy.get_param('path_user')
+path_classifier=rospy.get_param('Clasificador')
+
+
+def image_callback(ros_data):
+    global state_current
+    global state_prev 
+    state_current=ros_data.data
+    if state_current^state_prev:
+        if state_current:
+            #print 'Imges Completed'
+            print 'Welcome Sir '+Classification.user_recognized(path_user,path_classifier)
+        else:
+            print 'No user user detected'
+    state_prev=ros_data.data
+
+def main():
+    global state_current
+    global state_prev
+    state_prev=True
+    state_current=False
+    rospy.init_node('recognition_node')
+    image_topic = '/user_images'
+    rospy.Subscriber(image_topic, Bool, image_callback,queue_size=1)
+    #recognition = rospy.Publisher('/user_name', String, queue_size=10)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
