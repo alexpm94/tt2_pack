b3ddbff72e345f55333a07c5fa15ec102511c92b
alexpm94 <j.alejandro.pena.m94@gmail.com>
2018-04-24 17:55:22 -0500

    Solve path problems. Use rospack.get_path() instead of os.cwd()

--- src/tt2_pack/new_user.py
+++ src/tt2_pack/new_user.py
@@ -1,38 +1,44 @@
 import csv
 import os
+import rospkg 
+
+rospack = rospkg.RosPack()
 
 class Usuario:
 
 	def __init__(self,name):
 		self.name=name
+		self.users_csv=rospack.get_path('tt2_pack')+'/src/include/users.csv'+str(self.id_num)
 		self.get_id()
 		self.append_toCSV()
 		self.create_dir()
 
 	def get_id(self):
 		try: 
-			with open('names.csv', "rb") as f:
+			with open(self.users_csv, "rb") as f:
 				reader = csv.reader(f)
 				for last in reader: pass      # Loop through the whole file reading it all.
 				line=last
 				self.id_num=int(line[-1])+1
 				return self.id_num
 		except IOError:
-			with open('names.csv', 'a') as csvfile:
+			with open(self.users_csv, 'a') as csvfile:
 				fieldnames = ['first_name', 'id_number']
 				writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 				writer.writeheader()
 				self.id_num=0
 				return self.id_num
 
-
 	def append_toCSV(self):
-		with open('names.csv', 'a') as csvfile:
+		with open(self.users_csv, 'a') as csvfile:
 			fieldnames = ['first_name', 'id_number']
 			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 			writer.writerow({'first_name': self.name, 'id_number': self.id_num})
 
 	def create_dir(self):
-		directory=os.getcwd()+'/s'+str(self.id_num)
-		if not os.path.exists(directory):
-			os.makedirs(directory)
+		self.directory=rospack.get_path('tt2_pack')+'/src/dataBase/s'+str(self.id_num)
+		if not os.path.exists(self.directory):
+			os.makedirs(self.directory)
+
+	def getDirectory(self):
+		return self.directory
