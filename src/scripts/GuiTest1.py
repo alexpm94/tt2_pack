1afd9a7279d65b301d3b0908c03e56b4dbf8a0f3
alexpm94 <j.alejandro.pena.m94@gmail.com>
2018-05-16 19:31:24 -0500

    Fix bug apending names.

--- src/scripts/GuiTest1.py
+++ src/scripts/GuiTest1.py
@@ -220,7 +220,7 @@ def verificar0():
     #Correcto
     pattern=r"Hola "
     if re.match(pattern,nameGui):
-        nameGui=re.sub(pattern,"",str)
+        nameGui=re.sub(pattern,"",nameGui)
     Nombre=nameGui
     Estado='Correcto'
     Estado2=0
@@ -232,7 +232,7 @@ def verificar1():
     #Incorrecto
     pattern=r"Hola "
     if re.match(pattern,nameGui):
-        nameGui=re.sub(pattern,"",str)
+        nameGui=re.sub(pattern,"",nameGui)
     Nombre=nameGui
     Estado='Incorrecto'
     Estado2=1
